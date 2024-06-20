ca83cf64eba139b516607bb93b317fe81ecd7226
Abigail-DePriest <abby.depriest@gmail.com>
2024-06-20 18:51:24 -0500

    small changes to index

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import PostCard from '../../components/PostCard';
